375eee0dcbe403666481c0f1e5f1bd42ada2d8aa
Rajat Gupta <rjtgupta84@gmail.com>
2023-04-26 22:10:15 +0530

    protobuf: move from sprintf to snprintf for M chip mac
    Change-Id: I536d6afc1b2fcf7ea7ac2c0cdf6f665317573a2a
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- src/google/protobuf/io/strtod.cc
+++ src/google/protobuf/io/strtod.cc
@@ -63,7 +63,7 @@ std::string LocalizeRadix(const char* input, const char* radix_pos) {
   // to divuldge the locale's radix character.  No, localeconv() is NOT
   // thread-safe.
   char temp[16];
-  int size = sprintf(temp, "%.1f", 1.5);
+  int size = std::snprintf(temp, sizeof(temp), "%.1f", 1.5);
   GOOGLE_CHECK_EQ(temp[0], '1');
   GOOGLE_CHECK_EQ(temp[size - 1], '5');
   GOOGLE_CHECK_LE(size, 6);
